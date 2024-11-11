a9f497373ef88a2002feb0c2cbe7f6c6341f3949
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-11 09:53:17 +0200

    Exercise done

---
+++ basics.js
@@ -0,0 +1,17 @@
+const firstName  = "Sivuyile";
+
+const lastName = "Mtwetwe";
+
+let age = 24;
+
+console.log(age)
+
+const fullName = firstName + " " + lastName
+
+console.log("Hello"+ " " + fullName);
+
+console.log(`Hello ${fullName}`);
+
+age = 29
+
+console.log(age)
