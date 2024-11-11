8428ea7c4e1371bb1bd839b9383a3ca3bcfecadf
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-11 10:37:22 +0200

    Exercise done

---
+++ operations-types.js
@@ -0,0 +1,17 @@
+const number1 = 7;
+
+const number2 = 9;
+
+let num1String = String(number1);
+
+
+
+let sum = number1 + number2;
+
+let product = number1*number2;
+
+console.log(typeof num1String);
+
+console.log("The sum of number1 and number2 is" + " " + sum);
+
+console.log("The product of number1 and number2 is" + " " + product)
