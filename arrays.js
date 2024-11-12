8d2951fb56898606f071a448ad8f0987bc0ffa90
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-12 11:21:58 +0200

    exercise done

---
+++ arrays.js
@@ -0,0 +1,15 @@
+let colors = [];
+
+
+colors.push("Red","Blue","Green")
+
+let numbers = [5,10,15,20,25]
+
+colors.pop()
+
+
+colors.unshift("Pink")
+
+console.log(numbers);
+console.log(colors);
+
