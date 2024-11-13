49c1f4616852671641035f7f2fd059e6d080af3b
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-13 10:53:02 +0200

    Exercise done

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(name){
+    return `Hello, ${name}!`
+}
+
+function add(x,y){
+    return x+y
+}
+
+let greeting = greet("Sivuyile");
+
+let addition = add(5,1)
+
+console.log(greeting+ "\n"+ addition);
