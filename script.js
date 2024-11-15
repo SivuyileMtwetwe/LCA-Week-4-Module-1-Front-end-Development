877d401198aed93eabfc6dfe6caec7f984cb0a3b
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-15 15:57:17 +0200

    exercise done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// function to create sum of the numbers from 1 to the number passed
 function sum(x) { 
     
     let sum = 0;
@@ -14,8 +15,10 @@ function sum(x) {
     }
 
 }
-// console.log(sum(5));
+console.log(sum(5));
 
+
+//function to calculate the factorial of the number passed
 function factorial(number){
     if(number == 0 || number == 1){
         return 1;
@@ -25,8 +28,10 @@ function factorial(number){
 }
 
 
-// console.log(factorial(6));
+console.log(factorial(4));
 
+
+//function to create calculations based on the number of arguments passed
 function funkyMath(a,b,c,d){
     if(arguments.length == 2){
         return b-a
@@ -41,9 +46,11 @@ function funkyMath(a,b,c,d){
 
 
 
-// console.log(funkyMath(8,2,3,5));
+console.log(funkyMath(8,2,3,5));
 
 
+
+//for loop to push odd numbers to an array
 let arr = [1, 2 , 33, 45, 6,44]
 let oddArr = []
 for (let i = 0; i<arr.length;i++){
@@ -55,15 +62,29 @@ for (let i = 0; i<arr.length;i++){
     
 }
 console.log(oddArr);
-
+//reverse the odd array so that I can be able to sort it.
 oddArr.reverse()
 
 console.log(oddArr);
+//Making sure that the array is sorted in ascending order
 oddArr.sort()
 
 console.log(oddArr);
 
-
+//creating an object
 let me ={
+    firstName: "Sivuyile",
+    lastName: "Mtwetwe",
+    age: 24,
+    favColor: "Blue",
+    dreamCar: "Ferrari 250 GT"
     
 }
+//adding a key with its value to an object
+me["favFood"] = "Chicken Biryani"
+
+console.log(me);
+//deleting a key and its value from an object
+delete me.age
+
+console.log(me);
