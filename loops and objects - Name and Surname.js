f797e396a3367c5c788f8ac5faf809cdd40f0ec1
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-18 16:04:11 +0200

    tbc

---
+++ loops and objects - Name and Surname.js
@@ -0,0 +1,11 @@
+let numbers = [3,6,9,12,15,18]
+
+for (let i=0 ; i<numbers.length; i++){
+    console.log(numbers[i]);
+}
+
+let sum =0
+
+for(let j=0; j<numbers.length;j++){
+    sum += j
+}console.log(sum);
