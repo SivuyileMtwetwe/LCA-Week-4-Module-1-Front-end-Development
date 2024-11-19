4577acd296c6b0e9f4a457747931df5f52fd2979
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-19 15:24:43 +0200

    tbc

--- loops and objects - Name and Surname.js
+++ loops and objects - Name and Surname.js
@@ -20,3 +20,25 @@ for(let k = 0 ; k<numbers.length;k++){
 
     
 }console.log(oddSum);
+
+
+// let friend1 = {
+//     name: "Siphenathi",
+//     nickname: "Nathi",
+//     favFoods: ["Chicken Biryani", "Rice and Chicken"],
+//     hobbies: ["Gardening", "Coding"]
+
+// }
+
+// console.log(`Hi my name is ${friend1.name}, and my friends call me ${friend1.nickname}. I always eat ${friend1.favFoods[0]} and ${friend1.favFoods[1]}. I love to do some ${friend1.hobbies[0]} on my spare time but what I have been doing a lot these days is ${friend1.hobbies[1]}`);
+
+function friend(name,nickname, favFoods, hobbies){
+    this.name = name,
+    this.nickname = nickname,
+    this.favFoods = favFoods,
+    this.hobbies = hobbies
+}
+
+let friend2 =  friend("Mila","Milz",["Spaghetti, African Salad"], ["Eating", "Sleeping"])
+
+console.log(friend2);
