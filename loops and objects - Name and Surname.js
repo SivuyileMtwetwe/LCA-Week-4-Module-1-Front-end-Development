5739d2b8ead796894094a5c1bf8c87186570e5f2
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-19 16:04:37 +0200

    exercise done

--- loops and objects - Name and Surname.js
+++ loops and objects - Name and Surname.js
@@ -22,15 +22,15 @@ for(let k = 0 ; k<numbers.length;k++){
 }console.log(oddSum);
 
 
-// let friend1 = {
-//     name: "Siphenathi",
-//     nickname: "Nathi",
-//     favFoods: ["Chicken Biryani", "Rice and Chicken"],
-//     hobbies: ["Gardening", "Coding"]
+let friend1 = {
+    name: "Siphenathi",
+    nickname: "Nathi",
+    favFoods: ["Chicken Biryani", "Rice and Chicken"],
+    hobbies: ["Gardening", "Coding"]
 
-// }
+}
 
-// console.log(`Hi my name is ${friend1.name}, and my friends call me ${friend1.nickname}. I always eat ${friend1.favFoods[0]} and ${friend1.favFoods[1]}. I love to do some ${friend1.hobbies[0]} on my spare time but what I have been doing a lot these days is ${friend1.hobbies[1]}`);
+console.log(`Hi my name is ${friend1.name}, and my friends call me ${friend1.nickname}. I always eat ${friend1.favFoods[0]} and ${friend1.favFoods[1]}. I love to do some ${friend1.hobbies[0]} on my spare time but what I have been doing a lot these days is ${friend1.hobbies[1]}`);
 
 function friend(name,nickname, favFoods, hobbies){
     this.name = name,
@@ -39,6 +39,36 @@ function friend(name,nickname, favFoods, hobbies){
     this.hobbies = hobbies
 }
 
-let friend2 =  friend("Mila","Milz",["Spaghetti, African Salad"], ["Eating", "Sleeping"])
+let friend2 = new friend("Mila","Milz",["Spaghetti", "African Salad"], ["Eating", "Soccer"])
 
-console.log(friend2);
+let friend3 = new friend("Juan","John Wick",["Burgers", "Steak"], ["Paragliding","Being Boring"])
+
+let friend4 = new friend("Zenande","Zen",["Umfino","Umngqusho"],["Sleeping","Studying"])
+
+// console.log(friend2);
+// console.log(friend3);
+// console.log(friend4);
+
+let friends = []
+
+friends.push(friend1,friend2,friend3,friend4)
+
+for(let i = 0 ; i<friends.length;i++){
+    console.log(friends[i]);
+}
+for (let k = 0; k < friends.length; k++) {
+    for (let j = 0; j < friends[k].hobbies.length; j++) {
+        if (friends[k].hobbies[j] === "Soccer") {
+            console.log(friends[k]);
+            
+        }
+    }
+}
+
+
+for(let z = 0 ; z< friends.length;z++){
+    friends[z].hobbies.push("Being a legend");
+    
+}
+
+console.log(friends);
