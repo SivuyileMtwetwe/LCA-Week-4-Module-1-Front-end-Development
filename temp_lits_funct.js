0bb968dca523e261066cdea97ce3523540528646
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-20 13:22:11 +0200

    fewydjndcujxk

---
+++ temp_lits_funct.js
@@ -0,0 +1,6 @@
+let string = `
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quae dignissimos molestias inventore, incidunt hic provident illo reiciendis aspernatur ullam temporibus soluta accusantium numquam aliquid, minima ab esse distinctio et.
+`
+
+
+console.log(string);
