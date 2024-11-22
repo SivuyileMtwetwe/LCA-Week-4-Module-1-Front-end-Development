5202a57da3089459491558c0a94a5b7be5863cf3
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-22 15:29:50 +0200

    fetch

--- async_promises.js
+++ async_promises.js
@@ -1,14 +1,33 @@
-let users
+const fetchData = async (userId) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(async () => {
+        try {
+          const response = await fetch("https://sivuyilemtwetwe.github.io/LCA-Week-4-Module-1-Front-end-Development/");
+          const data = await response.json();
+  
+          
+          const users = data.users;
+  
+          
+          const filteredData = users.filter(user => user.id === userId);
+  
+          if (filteredData.length > 0) {
+            resolve(filteredData[0]);
+          } else {
+            reject(new Error("User not found"));
+          }
+        } catch (error) {
+          reject(error);
+        }
+      }, 1500); 
+    });
+  };
+  
+fetchData(5) 
+    .then(userData => {
+      console.log("Fetched user data:", userData);
+    })
+    .catch(error => {
+      console.error("Error fetching data:", error);
+    });
 
-const  fetchData = async () => {
-    let api =  await fetch("")
-
-    let data = await api.json()
-
-    users = data
-console.log(users);
-
-}
-
-
-fetchData() 
