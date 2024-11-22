e4ebd20ad34b5ff8464f7258cf2f6f6c2862e333
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-22 16:19:23 +0200

    tbc

--- async_promises.js
+++ async_promises.js
@@ -1,33 +1,76 @@
-const fetchData = async (userId) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          const response = await fetch("https://sivuyilemtwetwe.github.io/LCA-Week-4-Module-1-Front-end-Development/");
-          const data = await response.json();
-  
-          
-          const users = data.users;
-  
-          
-          const filteredData = users.filter(user => user.id === userId);
-  
-          if (filteredData.length > 0) {
-            resolve(filteredData[0]);
-          } else {
-            reject(new Error("User not found"));
-          }
-        } catch (error) {
-          reject(error);
-        }
-      }, 1500); 
-    });
-  };
-  
-fetchData(5) 
-    .then(userData => {
-      console.log("Fetched user data:", userData);
-    })
-    .catch(error => {
-      console.error("Error fetching data:", error);
-    });
+// const fetchData = async (userId) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(async () => {
+//       try {
+//         const response = await fetch(
+//           "https://sivuyilemtwetwe.github.io/LCA-Week-4-Module-1-Front-end-Development/"
+//         );
+//         const data = await response.json();
+
+//         const users = data.users;
+
+//         console.log(users);
+
+//         users.forEach((item) => {
+//           console.log(
+//             `Hi, my name is ${item.name} and I am employee number ${item.id}. You can email me on ${item.email}. I was employed and started working from ${item.registrationDate}`
+//           );
+//         });
+
+//         const filteredData = users.filter((user) => user.id === userId);
+
+//         if (filteredData.length > 0) {
+//           resolve(filteredData[0]);
+//         } else {
+//           reject(new Error("User not found"));
+//         }
+//       } catch (error) {
+//         reject(error);
+//       }
+//     }, 1500);
+//   });
+// };
+
+// fetchData(1);
+
+
+
+
+const fetchPosts = async (userId) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      try {
+        const response = await fetch(
+           " https://jsonplaceholder.typicode.com/posts"
+        );
+        const data = await response.json();
+
+        const posts = data;
+
+        console.log(posts);
+
+        users.forEach((item) => {
+          console.log(item.body
+
+          );
+        });
+
+//         const filteredData = posts.filter((user) => console.log(user) );
+// console.log(filteredData);
+        // if (filteredData.length > 0) {
+        //   resolve(filteredData[0]);
+        // } else {
+        //   reject(new Error("User not found"));
+        // }
+      } catch (error) {
+        reject(error);
+      }
+    }, 1000);
+  });
+};
+
+fetchPosts();
+
+
+
 
