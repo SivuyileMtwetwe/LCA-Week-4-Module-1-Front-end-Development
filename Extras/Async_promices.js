a21034d76fa1a365651307c76ef37c8ad9930e29
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-22 10:39:17 +0200

    new exercise

--- Extras/Async_promices.js
+++ Extras/Async_promices.js
@@ -1,58 +1,77 @@
-// console.log(1);
+// // // console.log(1);
 
-// console.log(2);
+// // // console.log(2);
 
-// console.log(3);
+// // // console.log(3);
 
-// setTimeout(() => {
-//     console.log(4);
-// }, 2000)
+// // // setTimeout(() => {
+// // //     console.log(4);
+// // // }, 2000)
 
-// console.log(5); 
+// // // console.log(5); 
 
 
-// function waiting(){
-//     new Promise
+// // // function waiting(){
+// // //     new Promise
+// // // }
+// // let workout = true
+// // // let x  = new Promise((resolve,reject)=>{
+// // // if (workout == true) {
+// // //     resolve("Man\'s looking like Johnny Bravo!")
+// // // } else {
+// // //     reject("Hebana Eish")
+// // // }
+
+// // // })
+
+
+// // // x.then((message)=> console.log( message.split('').reverse().join("")))
+
+
+// // async function waiting(){
+// //     let x = await new Promise(()=>{
+// //     if (workout == true) {
+// //         resolve("Man\'s looking like Johnny Bravo!")
+// //     } else {
+// //         reject("Hebana Eish")
+// //     }
+// // })
+// // }
+
+// // // fetch("https://matthew-dean-brown.github.io/api-data/data.json")
+// // // .then((data)=> data.json())
+// // // .then((x)=>console.log(x))
+// // // .catch(err=> console.error(err))
+
+
+// // // async function fetchData(){
+// // //     let data = await fetch("https://matthew-dean-brown.github.io/api-data/data.json")
+// // //     data.json().then((x)=>console.log(x)) 
+// // // }
+// // const  fetchData = async () => {
+// //     let data = await fetch("https://matthew-dean-brown.github.io/api-data/data.json")
+// //     data.json().then((x)=>console.log(x)) 
+// // }
+
+
+
+// // fetchData()
+
+
+// function name(){
+//     let x = 5
 // }
-let workout = true
-// let x  = new Promise((resolve,reject)=>{
-// if (workout == true) {
-//     resolve("Man\'s looking like Johnny Bravo!")
-// } else {
-//     reject("Hebana Eish")
-// }
-
-// })
+//  console.log(name());
 
 
-// x.then((message)=> console.log( message.split('').reverse().join("")))
+//  let name = ()=>{
+//     let x = 5
 
 
-async function waiting(){
-    let x = await new Promise(()=>{
-    if (workout == true) {
-        resolve("Man\'s looking like Johnny Bravo!")
-    } else {
-        reject("Hebana Eish")
-    }
-})
-}
+//  }
 
-// fetch("https://matthew-dean-brown.github.io/api-data/data.json")
-// .then((data)=> data.json())
-// .then((x)=>console.log(x))
-// .catch(err=> console.error(err))
+let array = [1,2,8,4,0]
 
+array.sort().reduce()
 
-// async function fetchData(){
-//     let data = await fetch("https://matthew-dean-brown.github.io/api-data/data.json")
-//     data.json().then((x)=>console.log(x)) 
-// }
-const  fetchData = async () => {
-    let data = await fetch("https://matthew-dean-brown.github.io/api-data/data.json")
-    data.json().then((x)=>console.log(x)) 
-}
-
-
-
-fetchData()
+console.log(array);
